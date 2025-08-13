23fe83bc8b29c164e1a57f123f38a21582f81a97
kaykobadhossain <kaykobadhossaintanjil01@gmail.com>
2025-08-13 22:15:08 +0600

    state management using context api

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,93 @@
+import { useContext, useEffect, useState } from "react";
+import { createContext } from "react";
+import axios from 'axios'
+import {toast} from 'react-hot-toast'
+import {data, useNavigate} from 'react-router-dom'
+
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+
+
+export const AppContext = createContext();
+
+export const AppProvider = ({children}) =>{
+    const navigate = useNavigate();
+    const currency = import.meta.env.VITE_CURRENCY;
+
+    const [token,setToken] = useState(null);
+    const [user,setUser] = useState(null);
+    const [isOwner,setIsOwner] = useState(false);
+    const [showLogin,setShowLogin] = useState(false);
+    const [pickUpDate,setPickUpDate] = useState('');
+    const [returnDate,setReturnDate] = useState('');
+
+    const [cars,setCars] = useState([]);
+
+    //function to check if user is logged in
+
+    const fetchUser = async()=>{
+        try {
+            await axios.get('/api/user/data')
+            if(data.success){
+                setUser(data.user)
+                setIsOwner(data.user.role ==='owner')
+            }else{
+                navigate('/')
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    //function to fetch all cars from the server
+
+    const fetchCars = async() =>{
+        try {
+            const {data} = await axios.get('/api/user/cars')
+            data.success ? setCars(data.cars) :  toast.error(data.message)
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
+    //function to log out the user
+
+    const logout = () =>{
+        localStorage.removeItem('token')
+        setToken(null)
+        setUser(null)
+        setIsOwner(false)
+        axios.defaults.headers.common['Authorization'] = ''
+        toast.success("You have been logged out")
+    }
+
+    //useffect to retrive the token from localstorage
+
+    useEffect(()=>{
+        const token = localStorage.getItem('token')
+        setToken(token)
+        fetchCars()
+    },[])
+
+    //useeffect to fetch user data when token is availavle
+
+    useEffect(()=>{
+        if(token){
+            axios.defaults.headers.common['Authorization'] = `${token}`
+            fetchUser()
+        }
+    },[token])
+
+    const value ={
+        navigate, currency, axios , user, setUser, token, setToken, isOwner, setIsOwner, fetchUser, showLogin,setShowLogin, logout, fetchCars, cars, setCars, pickUpDate, setPickUpDate, returnDate, setReturnDate 
+    }
+    return(
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppContext = () =>{
+    return useContext(AppContext)
+}
