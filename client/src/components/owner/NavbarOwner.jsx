09d2c273931c4919b15f1dde2e230cb56b0c9e15
kaykobadhossain <kaykobadhossaintanjil01@gmail.com>
2025-08-17 19:49:07 +0600

    some small issue solved

--- client/src/components/owner/NavbarOwner.jsx
+++ client/src/components/owner/NavbarOwner.jsx
@@ -8,7 +8,7 @@ const NavbarOwner = () => {
   return (
     <div className=" flex items-center justify-between px-6 md:px-10 py-4 text-gray-500 border-b border-borderColor relative transition-all">
 
-        <Link to='\'>
+        <Link to='/'>
             <img src= {assets.logo} alt="comp logo" className="h-7"/>
         </Link>
         <p>Welcome {user.name || "Owner"}</p>
